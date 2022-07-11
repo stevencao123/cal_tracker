f1eaa05f1e1590bdaa1b35f3407b4e98297fdc1b
wca93 <steven_cao_2@sfu.ca>
2022-07-11 15:42:39 -0700

    inital commit

--- App.js
+++ App.js
@@ -1,11 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, Image } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+
+      <View style={styles.navbar}>
+
+        <Image source={require("./assets/logo.png")} resizeMode='cover' style={{width:50, height:50}}/>
+        <Text style={{marginRight: 100, fontFamily:'roboto', fontWeight: 'bold', fontSize:30, color: '#34b6cb'}}>Cal Track</Text>
+
+        <TouchableOpacity style={styles.button}>
+          <Text>Today</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button}>
+          <Text>Profile</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button}>
+          <Text>Account</Text>
+        </TouchableOpacity>
+      </View>
+
+      <View style={styles.body}>
+        
+      </View>
+    
     </View>
   );
 }
@@ -16,5 +36,30 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+
+  navbar: {
+    flex: 0.1,
+    flexDirection: 'row',
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderBottomWidth: 1,
+  },
+
+  body: {
+    flex: 0.9,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  button: {
+    padding: 10,
+    marginLeft:10, 
+    borderRadius: 10,
+    borderColor: '#000',
+    borderWidth: 0.5,
+    backgroundColor: '#34b6cb',
   },
 });
